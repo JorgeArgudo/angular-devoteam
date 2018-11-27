6e17d6b11d6622e34e0dde8d036516cface2a4ef
Jorge <jorge.argudo.pino@devoteam.com>
2018-11-27 17:06:32 +0100

    Ajustes de tests unitarios

--- src/app/layout/header/header.component.spec.ts
+++ src/app/layout/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { MenuComponent } from '../menu/menu.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +10,11 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ 
+        HeaderComponent,
+        MenuComponent
+      ],
+      imports: [ RouterTestingModule ]
     })
     .compileComponents();
   }));
@@ -21,5 +27,10 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should have as title 'angular-devoteam'`, () => {
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('angular-devoteam');
   });
 });
---
+++ src/app/test/prueba.spec.ts
@@ -0,0 +1,19 @@
+import {factorial} from './prueba'
+
+fdescribe('Test de la funcion factorial', () => {
+    it('should be 0 when n = 0', () => {
+        expect(factorial(0)).toEqual(0);
+    });
+
+    it('should be 1 when n = 1', () => {
+        expect(factorial(1)).toEqual(1);
+    });
+
+    it('should be 120 when n = 5', () => {
+        expect(factorial(5)).toEqual(120);
+    });
+
+    it('should be ? when n = -1', () => {
+        expect(factorial(-1)).toBeNull();
+    });
+});
