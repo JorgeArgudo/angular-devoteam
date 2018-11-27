573869cf00232f5765fe42c3679451c180cde5c7
Jorge <jorge.argudo.pino@devoteam.com>
2018-11-27 15:59:12 +0100

    Componentes iniciales Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
+import { HomeModule } from './home/home.module';
+import { TasksModule } from './tasks/tasks.module';
+import { AboutModule } from './about/about.module';
 
 @NgModule({
   declarations: [
@@ -10,8 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    LayoutModule,
+    HomeModule,
+    TasksModule,
+    AboutModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/layout/footer/footer.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dvt-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+  autor: string;
+  lugar: string;
+  empresa: string;
+  fecha: Date;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.autor = 'Jorge Argudo'
+    this.lugar = 'Madrid';
+    this.empresa = 'Devoteam';
+    this.fecha = new Date();
+  }
+
+}
