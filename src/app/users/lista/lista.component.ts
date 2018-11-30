36b7dd44d3bb696d328aa2afcfb4e86735a9bac9
Jorge <jorge.argudo.pino@devoteam.com>
2018-11-30 14:11:21 +0100

    Users con Api JSON

---
+++ src/app/users/lista/lista.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/users.model';
+import { UsersService } from 'src/app/services/users.service';
+
+@Component({
+  selector: 'dvt-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+export class ListaComponent implements OnInit {
+
+  oUser: Usuario;
+  aUsers: Array<Usuario>;
+
+  constructor(public usersSrv: UsersService) { }
+
+  ngOnInit() {
+    this.oUser = new Usuario();
+    this.aUsers = [];
+    this.getUser();
+  }
+
+  getUser() {
+    if (this.oUser.id) {
+      this.usersSrv.getUser(this.oUser.id).subscribe(
+        (response) => { this.oUser = response; }
+      );
+    } else {
+      this.usersSrv.getUsers().subscribe(
+        (response) => { this.aUsers = response; }
+      );
+    }
+  }
+
+  addUser() {
+    if (!this.oUser.id) { return false; }
+    this.usersSrv.addUser(this.oUser).subscribe(
+      (response) => { this.actualizar(response); }
+    );
+  }
+
+  modifyUser() {
+    if (!this.oUser.id) { return false; }
+    this.usersSrv.setUser(this.oUser.id, this.oUser).subscribe(
+      (response) => { this.actualizar(response); }
+    );
+  }
+
+  deleteUser() {
+    if (!this.oUser.id) { return false; }
+    this.usersSrv.deleteUser(this.oUser.id).subscribe(
+      (response) => { this.actualizar(response); }
+    );
+  }
+
+  private actualizar(data) {
+    console.log(data);
+    this.oUser = new Usuario();
+    this.getUser();
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiLibros: 'https://www.googleapis.com/books/v1/volumes?q=intitle:',
+  apiUsers: 'https://localhost:3000/users/'
 };
